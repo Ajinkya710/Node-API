222532ae081d561eaa5efeb1a70fd17a8cadad46
Ajinkya710 <ajinkyabuva7@gmail.com>
2023-05-22 15:03:19 +1000

    Initial commit

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 console.log("Hello");
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello Node API!");
 });
-    
-app.listen(3000, () => {
-    console.log("Listening to port 3000...");
-}
-)
 
+app.get('/blog', (req, res) => {
+    res.send("Hello Blog! My Name is BroCODE");
+});
+
+app.post('/product', (req, res) => {
+    console.log(req.body);
+});
+
+mongoose
+    .connect('mongodb://localhost:27017')
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(3000, () => {
+            console.log("Listening to port 3000...");
+        }
+        )
+    }).catch((error) => {
+        console.log(error);
+    })
